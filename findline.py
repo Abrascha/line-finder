fc260b50675193881d9db492906023c7f8ed5513
p.yang <372729617@qq.com>
2020-01-14 14:17:51 +0100

    ployfit version

--- findline.py
+++ findline.py
@@ -8,11 +8,36 @@ import cv2
 import math
 
 
+def fit_coordinate(image, fit_parameters):
+    slope,intercept = fit_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*3/5)
+    x1 = int((y1-intercept)/slope)
+    x2 = int((y2-intercept)/slope)
+    return np.array([x1,y1,x2,y2])
 
 
 
-def takeSecond(elem):
-    return elem[1]
+
+def average_slope_intercept(image, lines):
+    fit_right = [] 
+    fit_left = [] 
+    for line in lines:
+        x1,y1,x2,y2 = line.reshape(4)
+        slope,intercept = np.polyfit((x1,x2),(y1,y2),1)
+        if slope > 0:
+            fit_right.append((slope, intercept))
+        else:
+            fit_left.append((slope, intercept))
+
+    left_line = fit_coordinate(image, np.average(fit_left, axis=0))
+    right_line = fit_coordinate(image, np.average(fit_right, axis=0))
+    return np.array([left_line, right_line])
+    
+
+
+def takeElem(elem,num):
+    return elem[num]
 
 def polarTocartesian(r,theta):
     return [int(r/math.cos(theta)), 0, 0, int(r/math.sin(theta))]  ## x1,y1,x2,y2
@@ -32,8 +57,7 @@ def findline(image_input):
     low_threshold = 50
     high_threshold = 180
 
-    edges = cv2.Canny(blur_gray, low_threshold, high_threshold) 
-    # return edges
+    edges = cv2.Canny(blur_gray, low_threshold, high_threshold,L2gradient= True) 
     mask = np.zeros_like(edges)
     ignore_color = 255
     imshape = image.shape
@@ -48,52 +72,37 @@ def findline(image_input):
     vertices = np.array([[(0,imshape[0]),(imshape[1]/2 - imshape[1]/64,y_high), (imshape[1]/2 + imshape[1]/64,y_high), (imshape[1],imshape[0])]],dtype=np.int32)
     cv2.fillPoly(mask,vertices,ignore_color)
     masked_edges = cv2.bitwise_and(edges,mask)
+    
     # print(y_high)
 
     # Define the Hough transform parameters
     # Make a blank the same size as our image to draw on
     rho = 1
     theta = (np.pi/180)
-    threshold = 80
-    min_line_length = 20
-    max_line_gap = 5
+    threshold = 20
+    min_line_length = 10
+    max_line_gap = 2
     line_image = np.copy(image)*0 #creating a blank to draw lines on
 
     # Run Hough on edge detected image
+    # lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
+    # trans_thetas = [[round((y1-y2)/(x1-x2),0) for x1,y1,x2,y2 in line][0] for line in lines]
+    # trans_thetas_count = [trans_thetas.count(x) for x in trans_thetas]
+    # # print(trans_thetas_count)
+    # for i, line in enumerate(lines):
+    #     if trans_thetas_count[i] > 1:
+    #         for x1,y1,x2,y2 in line:
+    #             cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),2)
+    
+    
     lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
-    trans_thetas = [[round((y1-y2)/(x1-x2),0) for x1,y1,x2,y2 in line][0] for line in lines]
-    trans_thetas_count = [trans_thetas.count(x) for x in trans_thetas]
-    # print(trans_thetas_count)
-    for i, line in enumerate(lines):
-        if trans_thetas_count[i] > 1:
-            for x1,y1,x2,y2 in line:
-                cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
-    
-    
-    
-    # lines_polar = cv2.HoughLines(masked_edges, rho, theta, threshold, np.array([]))
-    # polar_round = [[[round(xr,0),round(xtheta,0)] for xr, xtheta in line][0] for line in lines_polar.tolist()]
-    # ## todo: [[540.0, 1.0], [530.0, 1.0], [540.0, 1.0], [30.0, 2.0], [20.0, 2.0], [10.0, 2.0], [530.0, 1.0], [530.0, 1.0], [50.0, 2.0], [530.0, 1.0], [530.0, 1.0]]
-    
-    # print(polar_round)
-    # print(sorted(polar_round,key=takeSecond))
+    # ployfit() -- slope,b -- fit_right(+),fit_left(-) -- find points in vertices -- cv2.line
+    fit_lines = average_slope_intercept(image,lines)
 
-    # #Iterate over the output "lines" and draw lines on the blank
-    # for line in polar_round:
-    #     xr,xtheta = line
-    #     x1,y1,x2,y2 = polarTocartesian(xr,xtheta)
-    #     print(x1,y1,x2,y2)
-    #     cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),3)
+    for line in fit_lines:
+        x1,y1,x2,y2 = line
+        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
 
-    # todo:filter detection separating lines
-    # separat_filter
-    # print([abs((y1-y2)/(x1-x2)) for x1,y1,x2,y2 in lines])
-    
-
-    # Iterate over the output "lines" and draw lines on the blank
-    # for line in lines:
-    #     for x1,y1,x2,y2 in line:
-    #         cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
 
     # Create a "color" binary image to combine with line image
     color_edges = np.dstack((edges, edges, edges)) 
@@ -114,8 +123,6 @@ def main():
         image_output = findline(image_input)
         plt.subplot(3,3,i+1)
         plt.imshow(image_output,aspect='auto')
-        # plt.show()
-        # exit()
 
     plt.show()
 
